c561c60c1f6d2e206862e9f22ca3c1f70613e68b
John Casabar <johncasabar@gmail.com>
2018-10-12 01:54:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,3 +37,12 @@ end
 def over?(board)
   draw?(board) || won?(board)
 end
+
+
+def over?(board)
+  draw?(board) || won?(board)
+end
+
+def winner(board)
+  if won?(board) then board[won?(board)[0]]
+end
