3c0f612def983ae5466aa2d948ff75141a333620
John Casabar <johncasabar@gmail.com>
2018-10-12 01:59:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,5 +43,9 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) then return board[won?(board)[0]]
+  if won?(board)
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
 end
